89325be06e0cac11ba316087c52a777a663aaf01
FamiaMehmood <137001094+FamiaMehmood@users.noreply.github.com>
2024-08-28 00:16:26 -0700

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,31 @@
+console.log("JAVASCRIPT");
+//DATA TYPES
+var a=55; //It is number(int) data type
+console.log(a);
+
+let b=20.1;//It is number (float)data type
+console.log(b);
+
+const c="Famia";//It is string data type
+console.log(c);
+
+let sym2=Symbol("Hussain");//It is symbol data type
+console.log(sym2);
+let StudentIn={
+    name:"Famia", age:22,uniname:"COMSATS",//It is object data
+}
+console.log(StudentIn);
+let physics=80;
+let OOPS=90;
+let PF=94;
+let  marks=[80,80,95,"huzaifa","Bilal"];//It is array data type
+console.log(marks);
+let InfoStud=[
+    { 
+        name:"Famia", age:22,uniname:"COMSATS"},
+            {Teachers:["sir noman","maam hifza","sir Dr kashif Bilal","sir Affaq Khan"]},
+]
+console.log(InfoStud);//It is array of object data type
+
+
+
